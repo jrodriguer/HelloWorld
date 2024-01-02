ece2c44c05cbfd41216716e8a0e14cbb46059ae3
Julio <juliorodriguezramirez@gmail.com>
2024-01-02 12:45:49 +0100

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Julio Rodriguez on 2/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
